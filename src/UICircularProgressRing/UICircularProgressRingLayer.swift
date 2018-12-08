da234c9627dc6c17c6e937f76e9dfaf21df90628
Luca <khadgar1@gmail.com>
2018-12-08 01:29:19 +0100

    Update UICircularProgressRingLayer.swift

--- src/UICircularProgressRing/UICircularProgressRingLayer.swift
+++ src/UICircularProgressRing/UICircularProgressRingLayer.swift
@@ -467,6 +467,7 @@ class UICircularProgressRingLayer: CAShapeLayer {
         context.setShadow(offset: valueKnobShadowOffset, blur: valueKnobShadowBlur, color: valueKnobShadowColor.cgColor)
         context.addPath(knobPath.cgPath)
         context.setLineCap(.round)
+        
         if knobWhiteBorder == false
         {
             context.setFillColor(valueKnobColor.cgColor)
@@ -477,9 +478,6 @@ class UICircularProgressRingLayer: CAShapeLayer {
         {
             context.setFillColor(valueKnobColor.cgColor)
             context.drawPath(using: .fill)
-            context.setLineWidth(2)
-            context.setStrokeColor(UIColor.white.cgColor)
-            context.drawPath(using: .stroke)
         }
         
         if knobGradient == true && gradientColors.count > 1 {
@@ -504,6 +502,16 @@ class UICircularProgressRingLayer: CAShapeLayer {
             
             drawGradient(gradient, start: gradientStartPosition,
                          end: gradientEndPosition, in: context)
+        }
+
+        if knobWhiteBorder == true
+        {
+            context.restoreGState()
+            context.saveGState()
+            
+            context.setLineWidth(2)
+            context.setStrokeColor(UIColor.white.cgColor)
+            context.drawPath(using: .stroke)
         }
         
         context.restoreGState()
